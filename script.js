63d31e54c6e3bdf6195f39bf81f35dd8869082bd
Zac Nolan <zac1i1nolan@gmail.com>
2023-06-21 17:01:21 +0100

    added scroll

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function about(){
+    window.scrollTo(0, 0);
     document.getElementById("book").style.display = "none";
     document.getElementById("gallery").style.display = "none";
     document.getElementById("about").style.display = "block";
@@ -6,12 +7,14 @@ function about(){
 
 }
 function book(){
+    window.scrollTo(0, 0);
     document.getElementById("about").style.display = "none";
     document.getElementById("gallery").style.display = "none";
     document.getElementById("book").style.display = "block";
     document.getElementById("title_img").src = "perils2.webp"
 }
 function gallery(){
+    window.scrollTo(0, 0);
     document.getElementById("book").style.display = "none";
     document.getElementById("about").style.display = "none";
     document.getElementById("gallery").style.display = "block";
