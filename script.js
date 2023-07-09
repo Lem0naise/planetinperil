a5a51fe905f4120f57fb2ac5ec6fd05ca186ebb0
Zac Nolan <zac1i1nolan@gmail.com>
2023-07-09 20:32:28 +0100

    Added images

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function book(){
 function gallery(){
     document.getElementById("gallery").scrollIntoView();
 }
+function openpics(pics){
+    document.getElementById("pics").style.display = "flex";
+    document.getElementById("pics").innerHTML = ""; // reset images
+    for (i=0;i<pics.length;i++){
+        let b = document.createElement("img");
+        b.src = "imgs/" + pics[i];
+        document.getElementById("pics").appendChild(b); // add all images
+    }
+}
+function undo(){
+    document.getElementById("pics").style.display = "none";
+}
